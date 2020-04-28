ba207c94d59e034109f1e492c46c95c62a95b951
TotNotTac <andorthekid@gmail.com>
2020-04-28 11:18:19 +0200

    Added a simple install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+error=0
+
+if command -v git >/dev/null 2>&1
+then
+    git --version
+else
+    echo "git not installed"
+    error=1
+fi
+
+if command -v emacs >/dev/null 2>&1
+then
+    emacsversion=$(emacs --version | head -n 1 | awk '{ print $3 }')
+    echo "emacs version $emacsversion"
+else
+    echo "emacs is not installed"
+    error=1
+fi
+
+if [ $error -eq 1 ]
+then
+    echo "dependency requirements were not met"
+    exit 1
+fi
+
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
