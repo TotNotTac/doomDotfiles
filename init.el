236bf639dcd0ad6d0ae1fa4eecc771a3e5654444
TotNotTac <andorthekid@gmail.com>
2020-08-29 09:35:37 +0200

    Add agenda capture template

--- init.el
+++ init.el
@@ -25,7 +25,9 @@
        (company +childframe)
        ;; helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +icons)                     ; a search engine for love and life
+       (ivy
+        +icons
+        +prescient)                     ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
